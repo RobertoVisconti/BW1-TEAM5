9ba645ae971360ef5a0ffd767334f757f466c885
antonio.schettini <antonio.schettini93@gmail.com>
2026-03-24 12:25:49 +0100

    commit fix extrapage and add lots of js

--- scripts/extrapage.js
+++ scripts/extrapage.js
@@ -1,4 +1,33 @@
-const feedback = document.getElementById("feedaback")
-const feedbackList = function(){
-    feedback.createElement("p")
+const container = document.querySelector("footer div");
+// Recupero tutto l'array
+const tuttiIFeedback = JSON.parse(localStorage.getItem("listaFeedback")) || [];
+
+container.innerHTML = ""; // Pulisco il testo di prova
+
+for (let i = 0; i < tuttiIFeedback.length; i++) {
+  const fb = tuttiIFeedback[i];
+  const boxFeedback = document.createElement("div");
+
+  // Usiamo la tua classe CSS del box
+  boxFeedback.className = "span-domanda";
+  boxFeedback.style.marginBottom = "20px";
+  boxFeedback.style.display = "block";
+
+  // Costruiamo il contenuto del box
+  const stelleGrafiche = "Voto: " + fb.stelle + "/10";
+
+  boxFeedback.innerHTML =
+    "<div class='box-feedback'>" +
+    "<h2>" +
+    stelleGrafiche +
+    "</h2>" +
+    "<p>" +
+    fb.testo +
+    "</p>" +
+    "<p>" +
+    fb.data +
+    "</p>";
+  +"</div>";
+
+  container.appendChild(boxFeedback);
 }
